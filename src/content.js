45e50a6f8da9d511deecf9a657bc3663626ecdb9
codernami <codernami@gmail.com>
2022-07-22 18:17:45 -0300

    Adding content with data cards

---
+++ src/content.js
@@ -0,0 +1,26 @@
+const content = [
+{
+	id: 1,
+	title: 'Sedans',
+	text: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
+	image: 'icon-sedans.svg',
+},
+{
+	id: 2,
+	title: 'SUVs',
+	text: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
+	image: 'icon-suvs.svg',
+},
+{
+	id: 3,
+	title: 'Luxury',
+	text: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+	image: 'icon-luxury.svg',
+}
+]
+
+export default content;
+
+
+
+     
