7fe22df9b7e6e2a7684c3b807d887571ec8b84ce
codernami <codernami@gmail.com>
2022-07-24 14:12:27 -0300

    Adding props data

--- src/content.js
+++ src/content.js
@@ -4,18 +4,24 @@ const content = [
 	title: 'Sedans',
 	text: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
 	image: 'icon-sedans.svg',
+	bgColor: 'hsl(31, 77%, 52%)',
+	btnColor: 'hsl(31, 77%, 52%)',
 },
 {
 	id: 2,
 	title: 'SUVS',
 	text: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
 	image: 'icon-suvs.svg',
+	bgColor: 'hsl(184, 100%, 22%)',
+	btnColor: 'hsl(184, 100%, 22%)',
 },
 {
 	id: 3,
 	title: 'Luxury',
 	text: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
 	image: 'icon-luxury.svg',
+	bgColor: 'hsl(179, 100%, 13%)',
+	btnColor: 'hsl(179, 100%, 13%)',
 }
 ]
 
