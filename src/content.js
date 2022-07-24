1087f2d888b290f5dc7dcc8be01734c7505f6ac0
codernami <codernami@gmail.com>
2022-07-24 14:47:47 -0300

    Adding description to images in content file

--- src/content.js
+++ src/content.js
@@ -4,6 +4,7 @@ const content = [
 		title: 'Sedans',
 		text: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
 		image: 'icon-sedans.svg',
+		description: 'Sedan car',
 		bgColor: 'hsl(31, 77%, 52%)',
 		btnColor: 'hsl(31, 77%, 52%)',
 	},
@@ -12,6 +13,7 @@ const content = [
 		title: 'SUVS',
 		text: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
 		image: 'icon-suvs.svg',
+		description: 'Suv car',
 		bgColor: 'hsl(184, 100%, 22%)',
 		btnColor: 'hsl(184, 100%, 22%)',
 	},
@@ -20,6 +22,7 @@ const content = [
 		title: 'Luxury',
 		text: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
 		image: 'icon-luxury.svg',
+		description: 'Luxury car',
 		bgColor: 'hsl(179, 100%, 13%)',
 		btnColor: 'hsl(179, 100%, 13%)',
 	}
